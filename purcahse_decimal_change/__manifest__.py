bbc0a0ad8af29b9caed856f67480990e352df739
OdooAdmin1 <91454518+OdooAdmin1@users.noreply.github.com>
2021-11-21 12:33:23 +0300

    Add files via upload

---
+++ purcahse_decimal_change/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Purcahse Decimal Change",
+    'summary': """**""",
+    'description': """**""",
+    'author': "Vanshika",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '14.1',
+    'depends': ['base', 'purchase'],
+    'data': [
+        'views/purchase_order.xml',
+    ],
+    # only loaded in demonstration mode
+}
