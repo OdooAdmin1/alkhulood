c6344254d2d619d603ab3cffac1bb67562ac216e
shilpamariya <shilpa@zbeanztech.com>
2023-04-12 10:24:03 +0530

    [UPD] T1201,T1271,T1073 pos order migration,check printing and vat report issue fix

---
+++ pos_order_import_version_16/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "POS Order Import ",
+    'summary': """**""",
+    'description': """**""",
+    'author': "zesty beanz",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '16.0',
+    'depends': ['base','point_of_sale'],
+    'data': [
+
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+    # only loaded in demonstration mode
+}
---
+++ zb_custom_check_print/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+##############################################
+#
+# Inforise IT & ZestyBeanz Technologies Pvt. Ltd
+# By Dianne Jose(dianne@zbeanztech.com, dianne@inforiseit.com)
+# First Version 2019-04-03
+
+# Website1 : http://www.zbeanztech.com
+# Website2 : http://www.inforiseit.com
+
+###############################################
+
+{
+    "name": "Al-Khulood Custom",
+    "version": "16.0.01",
+    "author": "ZestyBeanz Technology Solution",
+    "website": "http://www.zbeanztech.com",
+    "description": """Al-Khulood Customization""",
+    "depends": ['account'],
+    "init_xml": [],
+    "demo": [],
+    "data": [
+                    'report/cheque_report.xml',
+                    'report/report.xml',
+
+                      ],
+    "auto_install": False,
+    "installable": True,
+    'application': True
+}
