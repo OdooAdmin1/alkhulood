21a8f1b8f5d2d020890b657a8041a1a7790794e1
shilpamariya <shilpa@zbeanztech.com>
2023-06-19 10:48:21 +0530

    [UPD] T1479 Need to Uncheck Allow Reconciliation

--- purcahse_decimal_change/models/purchase_order.py
+++ purcahse_decimal_change/models/purchase_order.py
@@ -4,14 +4,26 @@ from odoo import models, fields, api
 
 
 class PurchaseOrder(models.Model):
-	_inherit = 'purchase.order'
-	
-	amount_total = fields.Float(string='Total',
-		store=True, readonly=True, compute='_amount_all', digits=(16,4))
-	curr = fields.Char(string='', default="BD", readonly=True)
+    _inherit = 'purchase.order'
+
+    amount_total = fields.Float(string='Total',
+                                store=True, readonly=True, compute='_amount_all', digits=(16, 4))
+    curr = fields.Char(string='', default="BD", readonly=True)
+
 
 class PurchaseOrderLine(models.Model):
-	_inherit = 'purchase.order.line'
+    _inherit = 'purchase.order.line'
 
-	price_unit = fields.Float(string='Unit Price',
-		required=True, digits=(16,4))
+    price_unit = fields.Float(string='Unit Price',
+                              required=True, digits=(16, 4))
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    @api.model
+    def action_reset_account(self):
+        result = self.search([('account_id', 'in', [5, 6]), ('matching_number', '!=', False)], limit=50000)
+        for l in result:
+            l.remove_move_reconcile()
+        return True
